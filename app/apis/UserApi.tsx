97d15429caadde0c35987ce641120df4883fa481
viethoang2504 <hoangcongtu2409@gmail.com>
2024-12-09 16:43:40 +0700

    notification

--- app/apis/UserApi.tsx
+++ app/apis/UserApi.tsx
@@ -6,8 +6,8 @@ import axiosInstance from './apiConfig';
 //sau khi login, AsyncStorage sẽ lưu lại token, name, id, role
 export const getUserInfo = async (): Promise<UserInfo | null> => {
   try {
-    // const token = AsyncStorage.getItem('token');
-    // const userId = AsyncStorage.getItem('id');
+    // const token = await AsyncStorage.getItem('token');
+    // const userId = await AsyncStorage.getItem('id');
     const response = await axiosInstance.post('/it4788/get_user_info', {
       //user_id: id,
       user_id: 277,
