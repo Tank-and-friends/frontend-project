742eec3abf244db2633404dec1fa7f283ee6e58f
Nguyễn Trọng Đức <trongduc2003ht@gmail.com>
2024-12-12 21:14:33 +0700

    feat: config response

--- app/apis/apiConfig.tsx
+++ app/apis/apiConfig.tsx
@@ -1,5 +1,6 @@
+import AsyncStorage from '@react-native-async-storage/async-storage';
 import axios from 'axios';
-import {Alert} from 'react-native';
+import { Alert } from 'react-native';
 
 const axiosInstance = axios.create({
   baseURL: 'http://157.66.24.126:8080',
@@ -54,6 +55,31 @@ axiosInstance.interceptors.response.use(
       }
     }
     return Promise.reject(error); // Để giữ lỗi tiếp tục được truyền qua catch
+  },
+);
+
+axiosInstance.interceptors.request.use(
+  async (config: any) => {
+    try {
+      const token = await AsyncStorage.getItem('token');
+      if (token) {
+        // Kiểm tra nếu phương thức request cho phép body
+        if (['POST', 'PUT', 'PATCH'].includes(config.method?.toUpperCase())) {
+          // Nếu body đã có thì thêm thông tin vào
+          config.data = {
+            ...config.data, // Dữ liệu hiện tại
+            token: token, // Chèn thêm token
+          };
+        }
+      }
+      return config;
+    } catch (error) {
+      console.error('Error retrieving token:', error);
+      return Promise.reject(error);
+    }
+  },
+  error => {
+    return Promise.reject(error);
   },
 );
 
