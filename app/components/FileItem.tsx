6c90b88fb85f23c287bc3db6d646f01450118e04
Nguyễn Trọng Đức <trongduc2003ht@gmail.com>
2024-12-14 17:54:06 +0700

    feat: FileItem

---
+++ app/components/FileItem.tsx
@@ -0,0 +1,66 @@
+import React from 'react';
+import { Pressable, StyleSheet, Text, View } from 'react-native';
+import FontAwesome6Icon from 'react-native-vector-icons/FontAwesome6';
+import { FileInfo } from '../types/file';
+import { useToggle } from '../utils/useToggle';
+
+interface Props {
+  file: FileInfo;
+}
+
+export const FileItem = ({file}: Props) => {
+  const {
+    value: isOpenPreview,
+    setFalse: closePreview,
+    setTrue: openPreview,
+  } = useToggle(false);
+
+  return (
+    <>
+      <Pressable>
+        <View style={styles.itemContainer}>
+          <Pressable style={styles.uploadArea} onPress={openPreview}>
+            <FontAwesome6Icon name="file" color="#46515f" size={16} />
+            <Text style={styles.uploadContent}>
+              File {file.title.toLowerCase()}
+            </Text>
+          </Pressable>
+        </View>
+      </Pressable>
+
+      {isOpenPreview && (<></>)}
+    </>
+  );
+};
+
+const styles = StyleSheet.create({
+  itemContainer: {
+    padding: 10,
+    marginBottom: 10,
+    backgroundColor: '#fff',
+    borderRadius: 10,
+    flexDirection: 'row',
+    alignItems: 'center',
+    shadowColor: '#000',
+    shadowOffset: {width: 0, height: 0},
+    shadowOpacity: 0.1,
+    shadowRadius: 8,
+    elevation: 5,
+  },
+  uploadArea: {
+    width: '100%',
+    paddingVertical: 16,
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+    gap: 10,
+    borderWidth: 1,
+    borderColor: '#46515f',
+    borderRadius: 8,
+    borderStyle: 'dashed',
+  },
+  uploadContent: {
+    fontSize: 16,
+    color: '#46515f',
+  },
+});
